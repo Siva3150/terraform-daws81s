b2ad882e4d6be02d624aa5c46c28bc408bdb465b
siva3150 <siva123@gmail.com>
2024-09-17 09:45:16 +0530

    data-source

---
+++ data-sources/provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.67.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
+
